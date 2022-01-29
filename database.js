91cf0feb2fe79da8836b71a88704a7acddf658db
Mayar Ezz El-Din <55819809+MayarEzzel-Din@users.noreply.github.com>
2022-01-30 01:59:12 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 
-main().then(console.log('connected')).catch(err => console.log(err));
-async function main() {
-  await mongoose.connect("mongodb+srv://admin:admin@cluster0.pqf8w.mongodb.net/urlShortner?retryWrites=true&w=majority");
-}
 // instantiate a mongoose schema
 const dbSchema = new mongoose.Schema({
     url_ID: String,
